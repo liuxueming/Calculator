6dbedd3df86075e1949dd722b950030f6465063d
xueming.liu <xueming.liu@thoughtworks.com>
2019-07-09 20:43:23 +0800

    [功能]按钮增加tag

--- Calculator/View/ViewController.swift
+++ Calculator/View/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var allClear: RoundCornerButton!
+    @IBOutlet weak var resultDisplay: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func clickButton(_ sender: RoundCornerButton) {
+        print(sender.tag)
+    }
+    
 }
 
