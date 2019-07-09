10edbb11626283b6e78b7f68df09add9d7b6d769
xueming.liu <xueming.liu@thoughtworks.com>
2019-07-09 17:35:56 +0800

    [功能]自定义一个可以设置圆形圆角的类

---
+++ Calculator/View/RoundCornerButton.swift
@@ -0,0 +1,27 @@
+//
+//  RoundCornerButton.swift
+//  Calculator
+//
+//  Created by xueming.liu on 2019/7/9.
+//  Copyright © 2019 xueming.liu. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class RoundCornerButton: UIButton
+{
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        updateCornerRadius()
+    }
+    
+    @IBInspectable var rounded: Bool = false {
+        didSet {
+            updateCornerRadius()
+        }
+    }
+    
+    func updateCornerRadius() {
+        layer.cornerRadius = rounded ? frame.size.height / 2 : 0
+    }
+}
