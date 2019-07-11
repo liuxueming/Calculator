c3c009355d9a8d387d64976f6db97e1a6171591b
xueming.liu <xueming.liu@thoughtworks.com>
2019-07-11 08:35:03 +0800

    [clean code]删除unused方法

--- Calculator/View/CustomAlertView.swift
+++ Calculator/View/CustomAlertView.swift
@@ -78,12 +78,4 @@ import UIKit
             self.isHidden = true
         }
     }
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
 }
