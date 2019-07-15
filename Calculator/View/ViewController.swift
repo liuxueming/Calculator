3aae4a179d6c9dce8625f69f087a45829df3d1d9
xueming.liu <xueming.liu@thoughtworks.com>
2019-07-15 19:55:29 +0800

    [重构]enum重构

--- Calculator/View/ViewController.swift
+++ Calculator/View/ViewController.swift
@@ -9,8 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-    enum CalculatorButton {
-        case zero
+    enum CalculatorButton: Int {
+        case zero = 1000
         case one
         case two
         case three
@@ -20,41 +20,20 @@ class ViewController: UIViewController {
         case seven
         case eight
         case nine
+        case dot
         case allClear
         case positiveAndNegativeSign
         case percent
-        case plus
-        case minus
-        case multiply
         case divisor
+        case multiply
+        case minus
+        case plus
         case equal
     }
     @IBOutlet weak var allClear: RoundCornerButton!
     @IBOutlet weak var resultDisplay: UILabel!
     var previousValue:CalculatorButton?
     var currentValue:CalculatorButton?
-    lazy var CalculatorButtonTags:Dictionary = { () -> [Int : ViewController.CalculatorButton] in
-        let buttonTags = [0 : CalculatorButton.zero,
-                          1 : CalculatorButton.one,
-                          2 : CalculatorButton.two,
-                          3 : CalculatorButton.three,
-                          4 : CalculatorButton.four,
-                          5 : CalculatorButton.five,
-                          6 : CalculatorButton.six,
-                          7 : CalculatorButton.seven,
-                          8 : CalculatorButton.eight,
-                          9 : CalculatorButton.nine,
-                          10 : CalculatorButton.allClear,
-                          11 : CalculatorButton.positiveAndNegativeSign,
-                          12 : CalculatorButton.percent,
-                          13 : CalculatorButton.one,
-                          14 : CalculatorButton.divisor,
-                          15 : CalculatorButton.multiply,
-                          16 : CalculatorButton.minus,
-                          17 : CalculatorButton.plus,
-                          18 : CalculatorButton.equal]
-        return buttonTags
-    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -74,7 +53,7 @@ class ViewController: UIViewController {
     }
     @IBAction func clickButton(_ sender: RoundCornerButton) {
         previousValue = currentValue
-        currentValue = CalculatorButtonTags[sender.tag]
+        currentValue = CalculatorButton(rawValue: sender.tag)
         if previousValue == CalculatorButton.divisor && currentValue == CalculatorButton.zero{
             showAlert()
         }
